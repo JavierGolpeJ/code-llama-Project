122e0bb181e583ee8d1b138180898828659bdfd9
axvelez <tony.sonic11@gmail.com>
2025-04-11 12:39:06 -0500

    added import requests

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
+
+
+def read_file(filename_path):
+    with open(filename_path, "r") as f:
+        return f.read()
+
+
+# if __name__ == "__main__":
+#     read_file("code_files/code1.txt")
+
 def chat_with_ollama(prompt):
     url = "http://localhost:11434/api/generate"
     data = {"model": "gemma3", "prompt": prompt, "stream": True}
@@ -7,5 +17,8 @@ def chat_with_ollama(prompt):
             if line:
                 print(line)
 
+
+#
+#
 if __name__ == "__main__":
-    chat_with_ollama("Tell me a joke.")
+    chat_with_ollama(read_file("code_files/code1.txt"))
