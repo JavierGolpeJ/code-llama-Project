c95844a7d903f5a72d552e1b825052f8843652bb
JavierGolpeJ10 <golpeju2@uwm.edu>
2025-04-13 23:24:14 -0500

    Added get_function_headers - this function gets a list of all the def in the text file given

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from traceback import print_exc
+
 import requests
 
 import json
@@ -18,20 +20,20 @@ def run_model(prompt):
     print(full_response)
 
 
-def get_code(file_name):
-    with open(file_name, "r") as file:
-        content = file.read()
-        print("gets to the file")
-        # print(content)
-    return content
-
 # Todo
 def output_response_dock(ai_response):
     pass
 
 # Todo
-def get_function_headers(file_content):
-    pass
+def get_function_headers(file_path):
+    function_headers = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            if line.strip().startswith("def"):
+
+                function_headers.append(line.strip(" :\n "))
+    return function_headers
 
 # Todo
 def extract_function_info(file_content):
@@ -42,14 +44,36 @@ def create_documentation_prompt(file_content):
     pass
 
 # Example usage:
-if __name__ == "__main__":
-    prompt_text = """
-    give me code comments for this code in this format at the top of each function:
-    @param
-    @feature
-    @return
-    
-    code starts here:
+# if __name__ == "__main__":
+#     prompt_text = """ give me code comments for this code in this format at the top of each function:
+#     @param
+#     @feature
+#     @return
+#
+#     code starts here:
+#     def fibonacci_sequence(n):
+#     a, b = 0, 1
+#     if n <= 0:
+#         return []
+#     elif n == 1:
+#         return [0]
+#     else:
+#         result = [0, 1]
+#         while len(result) < n:
+#             next_num = a + b
+#             if next_num < n:
+#                 result.append(next_num)
+#                 a, b = b, next_num
+#             else:
+#                 break
+#         return result
+#     """
+#     # prompt_text = "i want you to make comments for this code: \n" + get_code("test-1.txt")
+#     # print(prompt_text)
+#     run_model(prompt_text)
+#     print("model is done running...")
+
+prompt_text = """
     def fibonacci_sequence(n):
     a, b = 0, 1
     if n <= 0:
@@ -66,8 +90,8 @@ if __name__ == "__main__":
             else:
                 break
         return result
-    """
-    # prompt_text = "i want you to make comments for this code: \n" + get_code("test-1.txt")
-    # print(prompt_text)
-    run_model(prompt_text)
-    print("model is done running...")
+"""
+
+response = get_function_headers("test-1.txt")
+
+print(response)
