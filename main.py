789a173355bcee25a18390e35a2364bfaac0802b
JavierGolpeJ10 <golpeju2@uwm.edu>
2025-04-15 17:35:34 -0500

    Added get_function_headers - this function gets a list of all the def in the given text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from traceback import print_exc
 
 import requests
-
+import os
 import json
 
 def run_model(prompt):
@@ -25,15 +25,29 @@ def output_response_dock(ai_response):
     pass
 
 # Todo
-def get_function_headers(file_path):
+def get_function_headers(file_path, directory_name):
+    path = os.path.join(directory_name, file_path)
     function_headers = []
 
-    with open(file_path, "r") as file:
+    with open(path, "r") as file:
         for line in file:
-            if line.strip().startswith("def"):
-
+            if line.strip().startswith("public"):
                 function_headers.append(line.strip(" :\n "))
     return function_headers
+
+
+def extract_headers_from_directory(directory_name):
+    all_headers = {}
+
+    for dir_path, dir_names ,files in os.walk(directory_name):
+        for file in files:
+            if file.endswith(".java"):
+                relative_path = os.path.relpath(os.path.join(dir_path, file), directory_name)
+                headers = get_function_headers(relative_path, directory_name)
+                if headers:
+                    all_headers[relative_path] = headers
+
+    return all_headers
 
 # Todo
 def extract_function_info(file_content):
@@ -92,6 +106,9 @@ prompt_text = """
         return result
 """
 
-response = get_function_headers("test-1.txt")
+# response = get_function_headers()
+# print(response)
 
-print(response)
+directory = 'Code-text'
+headers_by_file = extract_headers_from_directory(directory)
+print(headers_by_file)
